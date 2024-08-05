639daad31d56d420aff5d6c2b80e3f6392b1be70
Para Sivatharman <para.sivatharman@cirrus.com>
2024-08-05 10:52:22 +0100

    Refactor to use utility module

--- utils.py
+++ utils.py
@@ -1,6 +1,11 @@
+import numpy as np
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
+from scipy.optimize import minimize
+from scipy.stats import norm
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import Matern
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 
 def preprocess_data(standardise=False):
@@ -47,3 +52,64 @@ def preprocess_data(standardise=False):
     # Carry out train/test split from the given training data
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     return X_train, X_test, y_train, y_test, train_df, test_df
+
+
+def expected_improvement(X, X_sample, Y_sample, gpr, xi=0.01):
+    mu, sigma = gpr.predict(X, return_std=True)
+    mu_sample = gpr.predict(X_sample)
+
+    sigma = sigma.reshape(-1, 1)
+
+    mu_sample_opt = np.max(mu_sample)
+
+    with np.errstate(divide='warn'):
+        imp = mu - mu_sample_opt - xi
+        Z = imp / sigma
+        ei = imp * norm.cdf(Z) + sigma * norm.pdf(Z)
+        ei[sigma == 0.0] = 0.0
+
+    return ei
+
+def propose_location(acquisition, X_sample, Y_sample, gpr, bounds, n_restarts=25):
+    dim = X_sample.shape[1]
+    min_val = 1
+    min_x = None
+
+    def min_obj(X):
+        return -acquisition(X.reshape(-1, dim), X_sample, Y_sample, gpr)
+
+    for x0 in np.random.uniform(bounds[:, 0], bounds[:, 1], size=(n_restarts, dim)):
+        res = minimize(min_obj, x0=x0, bounds=bounds, method='L-BFGS-B')
+        if res.fun < min_val:
+            min_val = res.fun
+            min_x = res.x
+
+    return min_x.reshape(-1, 1)
+
+def bayesian_optimisation(n_iters, sample_loss, bounds, x0, y0, gp_params):
+    """
+    Bayesian optimization for decision trees
+    :param n_iters: No. of bayesian optimization iterations
+    :param sample_loss: The function that trains and evaluates the decision trees. See `optimise_decision_tree`
+    :param bounds: The bounds of each hyperparameter of the decision tree
+    :param x0: Initial sample points
+    :param y0: Initial evaluation points
+    :param gp_params: GPR parameters (GPR is initialised with Matern kernel)
+    :return: All sampled hyperparamters combinations, evaluations of them, final GPR
+    """
+    X_sample = x0
+    Y_sample = y0
+
+    gpr = GaussianProcessRegressor(kernel=Matern(nu=2.5), **gp_params)
+
+    for i in range(n_iters):
+        gpr.fit(X_sample, Y_sample)
+
+        X_next = propose_location(expected_improvement, X_sample, Y_sample, gpr, bounds)
+        params = X_next.flatten()
+        Y_next = sample_loss(*params)
+
+        X_sample = np.vstack((X_sample, X_next.T))
+        Y_sample = np.vstack((Y_sample, Y_next))
+
+    return X_sample, Y_sample, gpr
